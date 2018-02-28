4cbc1611956f5ff26a933b8979e9834b37db95fc
sumanth.chilka <sumanth.chilka@zohocorp.com>
2018-02-28 11:03:46 +0530

    Version update

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='zcrmsdk',
-    version='0.0.1',
+    version='1.0.0',
 
     description='Zoho CRM SDK for Python developers',
     long_description=long_description,
