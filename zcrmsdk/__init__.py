4230ffb757e5859b6066b216c14628418f792cd2
sumanth.chilka <sumanth.chilka@zohocorp.com>
2019-07-31 11:17:01 +0530

    Adding AbstractZohoOAuthPersistence to auto load

--- zcrmsdk/__init__.py
+++ zcrmsdk/__init__.py
@@ -9,11 +9,11 @@ try:
     from .Org import ZCRMOrganization
     from .CLException import ZCRMException
     from .Utility import APIConstants
-    from .OAuthClient import ZohoOAuth,ZohoOAuthClient,ZohoOAuthTokens
+    from .OAuthClient import ZohoOAuth,ZohoOAuthClient,ZohoOAuthTokens,AbstractZohoOAuthPersistence
 except ImportError:
     from RestClient import ZCRMRestClient
     from Operations import ZCRMAttachment,ZCRMCustomView,ZCRMCustomViewCategory,ZCRMCustomViewCriteria,ZCRMEventParticipant,ZCRMField,ZCRMInventoryLineItem,ZCRMJunctionRecord,ZCRMLayout,ZCRMLeadConvertMapping,ZCRMLeadConvertMappingField,ZCRMLookupField,ZCRMModule,ZCRMModuleRelatedList,ZCRMModuleRelation,ZCRMNote,ZCRMPermission,ZCRMPickListValue,ZCRMPriceBookPricing,ZCRMProfile,ZCRMProfileCategory,ZCRMProfileSection,ZCRMRecord,ZCRMRelatedListProperties,ZCRMRole,ZCRMSection,ZCRMTax,ZCRMTrashRecord,ZCRMUser,ZCRMUserCustomizeInfo,ZCRMUserTheme
     from Org import ZCRMOrganization
     from CLException import ZCRMException
     from Utility import APIConstants
-    from OAuthClient import ZohoOAuth,ZohoOAuthClient,ZohoOAuthTokens
+    from OAuthClient import ZohoOAuth,ZohoOAuthClient,ZohoOAuthTokens,AbstractZohoOAuthPersistence
